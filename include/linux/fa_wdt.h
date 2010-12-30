f1dcc21d5011af104c26a25b320541d6638f03b5
Damjan Marion <dmarion@me.com>
2010-12-30 23:36:58 +0100

    arm add fa watchdog driver

---
+++ include/linux/fa_wdt.h
@@ -0,0 +1,13 @@
+/*
+ * Platform data definition for the Faraday watchdog driver
+ */
+
+#ifndef _FA_WDT_H
+#define _FA_WDT_H
+
+struct fa_wdt_platform_data {
+	unsigned int	clock;
+};
+
+#endif /* _FA_WDT_H */
+
